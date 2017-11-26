1688f91814d7c4d6c72b0abd2c7764d0a38718bb
Tobias Schmidt <tobias.schmidt82@gmail.com>
2017-11-26 01:03:57 +0100

    Improve everything

--- src/Results.js
+++ src/Results.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Results.css';
+import thumbs_up from './thumbs_up.svg';
+import tobiRating from './tobiRating.js'
 
 
 const row = (result, index) =>
     <li key={`row_${index ? index : Math.random(1, 100000)}`}>
         <img src={result.Poster} alt={`${result.Title} Poster`}/>
         <span>{result.Title}</span>
-        {result.Ratings && 
-            <ul className="ratings">
-                {result.Ratings.map((rating, j) =>
-                    <li key={`ratings_${j}_${index}`}>{rating.Source}: {rating.Value}</li>
-                )}
-            </ul>
-        }
+        <ul className="ratings">
+            {result.Ratings && result.Ratings.map((rating, j) =>
+                <li key={`ratings_${j}_${index}`}>{rating.Source}: {rating.Value}</li>
+            )}
+        </ul>
+        <img src={thumbs_up} className={`tobirating tobirating-${result.tobiRating}`} alt={`tobi rating: ${result.tobiRating}`}/>
+        
     </li>
 
 
 
-
 function Results(props) {
+    const formattedResults = tobiRating(props.results);
     return (
         <ul className="results">
-            {props.results.map((result, index) =>
-                result.Response !== 'False' ? row(result) : <li className='notfound' key={`nf_${index}`}>{result.Title} not found</li>
+            {formattedResults.map((result, index) =>
+                result.Response !== 'False' 
+                    ? row(result) 
+                    : <li className='notfound' key={`nf_${index}`}>{result.Title} not found</li>
             )}
         </ul>
     );
---
+++ src/tobiRating.js
@@ -0,0 +1,25 @@
+const tobiRating = (results) => {
+    
+    return results.map((res) => {
+        let tobiRating = 0;
+        
+        if(res.imdbRating) {
+            const value = parseFloat(res.imdbRating, 10);
+            if (value < 7) tobiRating--;
+            if (value >= 7.5) tobiRating++;
+            if (value >= 9) tobiRating++;
+        }
+
+        if(res.Ratings && res.Ratings[1] && res.Ratings[1].Source === 'Rotten Tomatoes') {
+            const value = parseInt(res.Ratings[1].Value, 10);
+            if (value < 70) tobiRating--;
+            if (value >= 80) tobiRating++;
+            if (value >= 90) tobiRating = tobiRating + 2;
+        }
+
+        res.tobiRating = tobiRating;
+        return res;
+    });
+}
+
+export default tobiRating;
