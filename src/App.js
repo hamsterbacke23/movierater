7be2cb04df9ed4905b526ba160934b4b8b382930
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-01-19 15:03:24 +0100

    Add comments to results and style everything

--- src/App.js
+++ src/App.js
@@ -12,25 +12,25 @@ import debounce from 'lodash.debounce';
 import arrayDiff from 'arraydiff';
 
 class App extends Component {
-
   apiKey = 'e0fa53bd'; // please create your own at omdbapi.com
   apiEndpoint = 'https://www.omdbapi.com/?t=';
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      content: 'Taxi Driver\nWild strawberries // ingmar bergman\nRan\nPsycho\nAliens',
-      lines : [],
+      content:
+        'Taxi Driver\nWild strawberries // ingmar bergman\nRan\nPsycho\nAliens',
+      lines: [],
       infos: [],
-      href: '',
+      href: ''
     };
 
     this.debouncedSubmit = debounce(this.handleSubmit, 1000);
   }
 
   handleChange(event) {
-    this.setState({content: event.target.value});
+    this.setState({ content: event.target.value });
     this.debouncedSubmit();
   }
 
@@ -38,19 +38,25 @@ class App extends Component {
     if (event) {
       event.preventDefault();
     }
-    
-    const lines = this.state.content.split('\n')
-      .filter((item) => item.trim() !== '' ); // get rid of empty lines
- 
-    this.setState({
-      lines: lines,
-    }, this.updateHref);
 
+    const lines = this.state.content
+      .split('\n')
+      .filter(item => item.trim() !== ''); // get rid of empty lines
+
+    this.setState(
+      {
+        lines: lines
+      },
+      this.updateHref
+    );
   }
 
   removeInfo(removeDiff) {
     const newInfos = this.state.infos.filter((info, index) => {
-      return index < (removeDiff.index) || index >= (removeDiff.index + removeDiff.howMany);
+      return (
+        index < removeDiff.index ||
+        index >= removeDiff.index + removeDiff.howMany
+      );
     });
     this.setState({
       infos: newInfos
@@ -61,92 +67,112 @@ class App extends Component {
     if (window.location.hash) {
       this.getInfoFromHash();
     }
-    window.addEventListener('hashchange', this.getInfoFromHash.bind(this), false);
+    window.addEventListener(
+      'hashchange',
+      this.getInfoFromHash.bind(this),
+      false
+    );
   }
 
   componentWillUnmount() {
-    window.removeEventListener('hashchange', this.getInfoFromHash.bind(this), false);
+    window.removeEventListener(
+      'hashchange',
+      this.getInfoFromHash.bind(this),
+      false
+    );
   }
 
   componentDidUpdate(previousProps, previousState) {
-    if(previousState.lines !== this.state.lines) {
-      
+    if (previousState.lines !== this.state.lines) {
       const diff = arrayDiff(previousState.lines, this.state.lines);
 
-      diff.filter(diff => diff.type === 'insert')
+      diff
+        .filter(diff => diff.type === 'insert')
         .map(insertDiff => this.searchInfos(insertDiff));
-    
-      diff.filter(diff => diff.type === 'remove')
+
+      diff
+        .filter(diff => diff.type === 'remove')
         .map(removeDiff => this.removeInfo(removeDiff));
     }
   }
 
   getInfoFromHash() {
-    const hashLines = window.location.hash.replace('#', '')
+    const hashLines = window.location.hash
+      .replace('#', '')
       .split(',')
       .map(item => decodeURIComponent(item));
     this.setState({
       lines: hashLines,
-      content: hashLines.join('\n'),
+      content: hashLines.join('\n')
     });
   }
 
-  searchInfos(insertDiff){
+  searchInfos(insertDiff) {
     this.setState({
-      showSpinner: true,
+      showSpinner: true
     });
     const linePromises = insertDiff.values.map((line, j) => {
-      const movieTitle = line.replace(/\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/, ''); // remove comments
+      const commentRegex = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/;
+      const commentMatches = line.match(commentRegex);
+      const comment = commentMatches ? commentMatches[0].replace('//', '') : '';
+
+      const movieTitle = line.replace(commentRegex, ''); // remove comments
       const uri = this.apiEndpoint + `${movieTitle}&apikey=${this.apiKey}`;
 
       return fetch(uri)
         .then(response => {
-          if(!response.ok) {
+          if (!response.ok) {
             const e = new Error('Something went wrong');
             throw e;
           }
           return response.json();
-        }).then(json => {
-          if(json.Response === 'False') {
+        })
+        .then(json => {
+          if (json.Response === 'False') {
             json.Title = line;
           }
+          json.Comment = comment;
           const newInfos = this.state.infos;
           // insert new items right away at correct position from insertDiff.index
           newInfos.splice(insertDiff.index + j, 0, json);
           this.setState({
-            infos: newInfos,
+            infos: newInfos
           });
           return json;
-        })
-      });
+        });
+    });
 
-      Promise.all(linePromises).then(() => {
+    Promise.all(linePromises)
+      .then(() => {
         this.setState({
-          showSpinner: false,
+          showSpinner: false
         });
         this.updateHref();
-      }).catch(function(err) {
+      })
+      .catch(function(err) {
         this.setState({
-          showSpinner: false,
+          showSpinner: false
         });
         console.log(err.message);
-      });;
-
-  } 
+      });
+  }
 
   updateHref() {
     if (window.history.pushState) {
       window.history.pushState(
-        {state: this.state.content}, null, '#' + this.state.lines.map(line => encodeURIComponent(line)).join());
+        { state: this.state.content },
+        null,
+        '#' + this.state.lines.map(line => encodeURIComponent(line)).join()
+      );
     }
     this.setState({
-      href: window.location.href,
-    })
+      href: window.location.href
+    });
   }
 
   onCopy(result) {
     this.setState({ showResultMsg: true });
-    
+
     this.copyTimeout = window.setTimeout(() => {
       window.clearTimeout(this.copyTimeout);
       this.setState({ showResultMsg: false });
@@ -154,41 +180,59 @@ class App extends Component {
   }
 
   render() {
-    document.title = this.state.infos.length 
-      ? 'Movierater - ' + this.state.infos.map(item => item.Title).join(', ') 
+    document.title = this.state.infos.length
+      ? 'Movierater - ' + this.state.infos.map(item => item.Title).join(', ')
       : 'Movierater';
-  
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Movierater</h1>
         </header>
         <form className="App-input" onSubmit={this.handleSubmit}>
-          <textarea onChange={this.handleChange.bind(this)} value={this.state.content}></textarea>
-          <Button type="submit" onClick={this.handleSubmit}>Submit</Button>
+          <textarea
+            onChange={this.handleChange.bind(this)}
+            value={this.state.content}
+          />
+          <Button type="submit" onClick={this.handleSubmit}>
+            Submit
+          </Button>
         </form>
-        
-        <div className={this.state.showSpinner ? 'spinner active' : 'spinner'}></div>
-        
-        <Results results={this.state.infos}/>
-        
-        {this.state.infos.length > 0 && 
-          <Copy 
+
+        <div
+          className={this.state.showSpinner ? 'spinner active' : 'spinner'}
+        />
+
+        <Results results={this.state.infos} />
+
+        {this.state.infos.length > 0 && (
+          <Copy
             textToBeCopied={this.state.href}
             onCopy={this.onCopy.bind(this)}
+            className="copywrapper"
           >
-            <Button level='secondary'>
-              {this.state.showResultMsg === true 
-                ? 'copied'
-                : <span className="flex">Copy Link<img src={chain} alt="chain icon" /></span>
-              }
+            <Button level="secondary">
+              {this.state.showResultMsg === true ? (
+                'Copied!'
+              ) : (
+                <span className="flex">
+                  Copy Link<img
+                    src={chain}
+                    className="icon chain-icon"
+                    alt="chain icon"
+                  />
+                </span>
+              )}
             </Button>
           </Copy>
-        }
+        )}
 
-        <a className="view-on-github" href="https://github.com/hamsterbacke23/movierater">
+        <a
+          className="view-on-github"
+          href="https://github.com/hamsterbacke23/movierater"
+        >
           <span> View on GitHub </span>
-          <img src={octocat} alt="octocat"/>
+          <img src={octocat} alt="octocat" />
         </a>
       </div>
     );
--- src/Results.js
+++ src/Results.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Results.css';
 import thumbsUp from './svg/thumbsUp.svg';
-import customRating from './customRating.js'
-
+import customRating from './customRating.js';
 
 const netflixSearchUri = 'https://www.netflix.com/search?q=';
 const imdbUri = 'https://www.imdb.com/title/';
@@ -11,63 +10,84 @@ const thumbsUpSteps = 22;
 const thumbsUpStart = 120;
 const thumbsUpNullRating = 180;
 
-const row = (result, index) =>
-    <li key={`row_${index ? index : Math.random(1, 100000)}`}>
-        {result.Poster !== 'N/A' 
-        ? <img src={result.Poster} alt={`${result.Title} Poster`}/>
-        : <span className='noimage'>N/A</span>
-        }
+const row = (result, index) => (
+  <li key={`row_${index ? index : Math.random(1, 100000)}`}>
+    {result.Poster !== 'N/A' ? (
+      <img src={result.Poster} alt={`${result.Title} Poster`} />
+    ) : (
+      <span className="noimage">N/A</span>
+    )}
 
+    <div className="infowrapper">
+      <div className="info">
         <div>
-            <span className="title">{result.Title}</span>
-            <div className="links">
-              {result.imdbLink}
-              <a href={netflixSearchUri + encodeURIComponent(result.Title)}>Search on Netflix</a>
-            </div>
+          <span className="title">{result.Title}</span>
+          <div className="links">
+            {result.imdbLink}
+            <a href={netflixSearchUri + encodeURIComponent(result.Title)}>
+              Search on Netflix
+            </a>
+          </div>
         </div>
 
         <ul className="ratings">
-            {result.Ratings && result.Ratings.map((rating, j) =>
-                <li key={`ratings_${j}_${index}`}>
-                    <span className="vendor">{rating.Source}</span> 
-                    <span className="value">{rating.Value}</span>
-                </li>
-            )}
+          {result.Comment && (
+            <li className="comment">
+              <span className="value">{result.Comment}</span>
+            </li>
+          )}
+          {result.Ratings &&
+            result.Ratings.map((rating, j) => (
+              <li key={`ratings_${j}_${index}`}>
+                <span className="vendor">{rating.Source}</span>
+                <span className="value">{rating.Value}</span>
+              </li>
+            ))}
         </ul>
+
         <img
-            src={thumbsUp} 
-            className={`customRating customRating-${result.customRating}`} 
-            alt={`tobi rating: ${result.customRating}`}
-            style={{
-                'transform': `rotate(${result.customRating > 0 ? thumbsUpStart - thumbsUpSteps * result.customRating : thumbsUpNullRating}deg)`
-            }}
-            />
-        
-    </li>
+          src={thumbsUp}
+          className={`customRating customRating-${result.customRating}`}
+          alt={`tobi rating: ${result.customRating}`}
+          style={{
+            transform: `rotate(${
+              result.customRating > 0
+                ? thumbsUpStart - thumbsUpSteps * result.customRating
+                : thumbsUpNullRating
+            }deg)`
+          }}
+        />
+      </div>
+    </div>
+  </li>
+);
 
-const formatTitle = (results) => results.map((res) => {
-    const newTitle = res.imdbID 
-        ? <a href={imdbUri + res.imdbID}>imdb</a>
-        : '';
+const formatTitle = results =>
+  results.map(res => {
+    const newTitle = res.imdbID ? <a href={imdbUri + res.imdbID}>imdb</a> : '';
     return {
-        ...res,
-        imdbLink: newTitle
-    }
-});
-
+      ...res,
+      imdbLink: newTitle
+    };
+  });
 
 function Results(props) {
-    const customratedResults = customRating(props.results);
-    const formattedResults = formatTitle(customratedResults);
-    return (
-        <ul className="results">
-            {formattedResults.map((result, index) =>
-                result.Response !== 'False' 
-                    ? row(result) 
-                    : <li className='notfound' key={`nf_${index}`}>{result.Title} not found</li>
-            )}
-        </ul>
-    );
+  const customratedResults = customRating(props.results);
+  const formattedResults = formatTitle(customratedResults);
+  return (
+    <ul className="results">
+      {formattedResults.map(
+        (result, index) =>
+          result.Response !== 'False' ? (
+            row(result)
+          ) : (
+            <li className="notfound" key={`nf_${index}`}>
+              {result.Title} not found
+            </li>
+          )
+      )}
+    </ul>
+  );
 }
 
 Results.propTypes = {
